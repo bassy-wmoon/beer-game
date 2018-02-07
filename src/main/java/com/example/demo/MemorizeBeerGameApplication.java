1baf6b2f1ec83f7ec0c69ea630802337102cd49d
tom bassy <double.moon84@gmail.com>
2018-02-07 19:50:03 +0900

    initial commit

---
+++ src/main/java/com/example/demo/MemorizeBeerGameApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorizeBeerGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorizeBeerGameApplication.class, args);
+	}
+}
