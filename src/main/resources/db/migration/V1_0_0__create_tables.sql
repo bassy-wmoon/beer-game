6f679c811ecf455588c68ab8e9cf3671f844a2e1
tom bassy <double.moon84@gmail.com>
2018-02-14 21:32:45 +0900

    Add ddl for migration file

--- src/main/resources/db/migration/V1_0_0__create_tables.sql
+++ src/main/resources/db/migration/V1_0_0__create_tables.sql
@@ -6,9 +6,9 @@ CREATE TABLE if not exists `breweries` (
 ) COMMENT 'ブルワリー';
 
 CREATE TABLE if not exists `beer_styles` (
-  `style_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `beer_style_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id',
   `name` varchar(50) NOT NULL COMMENT 'スタイル名',
-  PRIMARY KEY (`style_id`),
+  PRIMARY KEY (`beer_style_id`),
   UNIQUE KEY `uk_beer_styles_name` (`name`)
 ) COMMENT 'ビアスタイル';
 
@@ -19,15 +19,24 @@ CREATE TABLE if not exists `beers` (
   `description` text(1000) COMMENT '説明',
   PRIMARY KEY (`beer_id`),
   UNIQUE KEY `uk_beers_name` (`name`),
-  FOREIGN KEY `fk_beer_style` (`style`) REFERENCES `beer_styles` (`style_id`)
+  FOREIGN KEY `fk_beer_style` (`style`) REFERENCES `beer_styles` (`beer_style_id`)
 ) COMMENT 'ビール';
 
 CREATE TABLE if not exists `beer_photos` (
-  `photo_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `url` varchar(50) NOT NULL COMMENT '写真のurl',
+  `beer_photo_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `url` varchar(200) NOT NULL COMMENT '写真のurl',
   `width` int(10) NOT NULL COMMENT '写真の横幅px',
   `height` int(10) NOT NULL COMMENT '写真の縦幅px',
-  PRIMARY KEY (`photo_id`),
+  PRIMARY KEY (`beer_photo_id`),
   UNIQUE KEY `uk_photo_url` (`url`),
-  FOREIGN KEY `fk_photo_id` (`photo_id`) REFERENCES `beers` (`beer_id`)
+  FOREIGN KEY `fk_photo_id` (`beer_photo_id`) REFERENCES `beers` (`beer_id`)
 ) COMMENT 'ビールの写真';
+
+CREATE TABLE if not exists `belgian_beers` (
+  `brewery_id` int(10) NOT NULL COMMENT 'ブルワリーid',
+  `beer_id` int(10) NOT NULL COMMENT 'ビールid',
+  PRIMARY KEY (`brewery_id`, `beer_id`),
+  FOREIGN KEY `fk_brewery_id` (`brewery_id`) REFERENCES `breweries` (`brewery_id`),
+  FOREIGN KEY `fk_beer_id` (`beer_id`) REFERENCES `beers` (`beer_id`)
+) COMMENT 'ベルギービール';
+
